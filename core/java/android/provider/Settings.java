7aa6ce981ec3516839a9d242aa01f6b59f716184
cphelps76 <cphelps76@droidconcepts.com>
2015-01-05 19:08:49 -0700

    Frameworks_base: Port BatteryBar [1/2]
    {x} Adds Battery bar with following features:
    {x} location set
    {x} color
    {x} aligment
    {x} thickness
    {x} charging animation
    
    Frameworks_base: update battery bar default color to match kitkat ui
    (1/2)
    
    Change-Id: I2f870451ccc73abbe410d51b57a3568b27659826
    Signed-off-by: STELIX <ssspinni@gmail.com>
    (cherry picked from commit 2d50722df4c09918ec4ed8ef8a403d1ef8f2fd05)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2107,6 +2107,32 @@ public final class Settings {
          */
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
+         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
         /**
          * The keyboard brightness to be used while the screen is on.
          * Valid value range is between 0 and {@link PowerManager#getMaximumKeyboardBrightness()}
