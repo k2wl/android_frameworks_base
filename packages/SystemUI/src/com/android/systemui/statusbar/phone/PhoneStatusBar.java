5cdaafc61160f012afad690506acdb7c70d249ef
FusionSP <dragon7780@hotmail.nl>
2015-02-26 16:54:05 -0700

    Detect status bar carrier state when theme change.
    This will fix the issue when changing or applying theme,
    the status bar carrier label was set to enabled.
    Then when we want to disable the status bar carrier label
    thru Settings the systemui would crash.
    
    now we let the system detect if the user has
    enabled or disabled the carrier label when we change or apply theme.
    
    Change-Id: I062ee85f3b4912ebecfb5e1e7ad6e4d222f044fa

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3909,6 +3909,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     void updateResources() {
         final Context context = mContext;
         final Resources res = context.getResources();
+        ContentResolver resolver = mContext.getContentResolver();
 
         // detect theme change.
         ThemeConfig newTheme = res.getConfiguration().themeConfig;
@@ -3916,6 +3917,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 (mCurrentTheme == null || !mCurrentTheme.equals(newTheme))) {
             mCurrentTheme = (ThemeConfig)newTheme.clone();
             recreateStatusBar();
+
+            // detect status bar carrier state when theme change.
+            mShowStatusBarCarrier = Settings.CMREMIX.getInt(
+                    resolver, Settings.CMREMIX.STATUS_BAR_CARRIER, 0) == 1;
+            showStatusBarCarrierLabel(mShowStatusBarCarrier);
+
         } else {
             loadDimens();
         }
