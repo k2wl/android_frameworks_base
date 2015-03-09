85bb06a917a18cc3da7ca984e2ee25edfdb7ecf1
westcripp <korukaltan@gmail.com>
2015-03-09 18:19:26 +0100

    QSColors: Fix empty notification alert after switch color option
    Change-Id: I9935864579c0d9a3ac7a0758cda076dc71e2c9b4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -528,6 +528,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                             Settings.CMREMIX.QS_COLOR_SWITCH,
                             0, UserHandle.USER_CURRENT) == 1;
                     recreateStatusBar();
+                    updateRowStates();
+                    updateSpeedbump();
+                    updateClearAll();
+                    updateEmptyShadeView();
             }
             update();
         }
