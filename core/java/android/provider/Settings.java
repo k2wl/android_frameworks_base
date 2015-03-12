1c36a6eeac39ef39a23b8ca925466c733cc8ccbe
ZION959 <ziontran@gmail.com>
2015-03-11 22:03:05 -0700

    Revert: FWB: LS weather disabler (1/2)
    PS2: No more FC
    
    Change-Id: I551fbbcb78712e8b04fcd316561f117693056bfc
    
    Conflicts: (reverted from commit 4f928d3dc259309fb130cb34bfcde000e4b6ea07)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9430,14 +9430,6 @@ public final class Settings {
         public static final String DIALER_WIDGET_HIDE = "dialer_widget_hide";
 
         /**
-         * Allows to disable weather in locskcreen
-         * 1 = use system default (show)
-         * 0 = hide Weather
-         * @hide
-         */
-        public static final String LOCKSCREEN_WEATHER = "lockscreen_weather";
-
-        /**
          * Disable FC Notifications
          * @hide
          */
